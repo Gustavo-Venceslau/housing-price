37400c97461de4779d5268a910b529fdfae5c5ea
Gustavo-de-Almeida <gustavodealmeida01@gmail.com>
2025-01-13 08:17:13 -0300

    separating the pipeline in another file

--- train.py
+++ train.py
@@ -1,78 +1,6 @@
-from dataset import housing
-import pandas as pd
-from sklearn.model_selection import StratifiedShuffleSplit
-from sklearn.impute import SimpleImputer
-from sklearn.base import BaseEstimator, TransformerMixin
+from pipeline import full_pipeline, housing, housing_labels, strat_test_set
 import numpy as np
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
-
-housing["income_cat"] = pd.cut(housing["median_income"],
-                              bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
-                               labels=[1, 2, 3, 4, 5]
-                              )
-
-# Divide the training set into training and validation sets
-split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
-for train_index, test_index in split.split(housing, housing["income_cat"]):
-    strat_train_set = housing.loc[train_index]
-    strat_test_set = housing.loc[test_index]
-
-# Remove the income_cat column
-for set_ in (strat_train_set, strat_test_set):
-    set_.drop("income_cat", axis=1, inplace=True)
-
-# Copy the training set
-housing = strat_train_set.copy()
-
-# Creating a clean training set for numeric columns and labels for prediction
-housing = strat_train_set.drop("median_house_value", axis=1)
-housing_labels = strat_train_set["median_house_value"].copy()
-
-# Deleting non-numeric column to add values to the nulls
-imputer = SimpleImputer(strategy="median")
-housing_num = housing.drop("ocean_proximity", axis=1)
-
-# Building our own transformer to fit our needs
-rooms_ix, bedrooms_ix, population_ix, households_ix = 3, 4, 5, 6
-
-class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
-    def __init__(self, add_bedrooms_per_room=True):
-        self.add_bedrooms_per_room = add_bedrooms_per_room
-    def fit(self, X, y=None):
-        return self
-    def transform(self, X, y=None):
-        rooms_per_household = X[:, rooms_ix] / X[:, households_ix]
-        population_per_household = X[:, population_ix] / X[:, households_ix]
-        if self.add_bedrooms_per_room:
-            bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
-            return np.c_[X, rooms_per_household, population_per_household, bedrooms_per_room]
-        else:
-            return np.c_[X, rooms_per_household, population_per_household]
-
-attr_adder = CombinedAttributesAdder(add_bedrooms_per_room=False)
-house_extra_attribs = attr_adder.transform(housing.values)
-
-# Creating a pipeline for the transformations we must do
-num_pipeline = Pipeline([
-    ('imputer', SimpleImputer(strategy="median")),
-    ('attribs_adder', CombinedAttributesAdder()),
-    ('std_scaler', StandardScaler()),
-])
-
-housing_num_tr = num_pipeline.fit_transform(housing_num)
-
-# Contem apenas os nomes dos atributos
-num_attribs = list(housing_num)
-cat_attribs = ["ocean_proximity"]
-
-full_pipeline = ColumnTransformer([
-    ("num", num_pipeline, num_attribs),
-    ("cat", OneHotEncoder(), cat_attribs)
-])
 
 housing_prepared = full_pipeline.fit_transform(housing)
 
@@ -115,7 +43,7 @@ squared_errors = (final_predictions - y_test) ** 2
 confidence_interval = np.sqrt(stats.t.interval(confidence, len(squared_errors) - 1,
                          loc=squared_errors.mean(),
                         scale=stats.sem(squared_errors)))
-						
+
 print(confidence_interval)
 
 # Saving the model
